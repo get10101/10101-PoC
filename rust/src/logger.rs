fef0cea03de844f40a181b906ccdd39bfdcd5ca0
Mariusz Klochowicz <mariusz@klochowicz.com>
2022-11-08 13:46:56 +1030

    feat(rust): Enable tracing support
    Add logging on the Rust side with tracing.
    Actual code for initialising tracing subsystem was pinched from automaker.

--- rust/src/logger.rs
+++ rust/src/logger.rs
@@ -1,5 +1,40 @@
+use anyhow::Result;
 use flutter_rust_bridge::StreamSink;
 use state::Storage;
+use time::macros::format_description;
+use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::fmt::time::UtcTime;
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+
+pub fn init_tracing(level: LevelFilter, json_format: bool) -> Result<()> {
+    if level == LevelFilter::OFF {
+        return Ok(());
+    }
+
+    let is_terminal = atty::is(atty::Stream::Stderr);
+
+    let filter = EnvFilter::from_default_env().add_directive(level.to_string().parse()?);
+
+    let builder = FmtSubscriber::builder()
+        .with_env_filter(filter)
+        .with_writer(std::io::stderr)
+        .with_ansi(is_terminal);
+
+    if json_format {
+        builder.json().with_timer(UtcTime::rfc_3339()).init()
+    } else {
+        builder
+            .compact()
+            .with_timer(UtcTime::new(format_description!(
+                "[year]-[month]-[day] [hour]:[minute]:[second]"
+            )))
+            .init()
+    };
+
+    tracing::info!("Initialized logger");
+
+    Ok(())
+}
 
 /// Wallet has to be managed by Rust as generics are not support by frb
 static LOG_STREAM_SINK: Storage<StreamSink<LogEntry>> = Storage::new();
@@ -11,13 +46,16 @@ pub struct LogEntry {
 }
 
 pub fn create_log_stream(sink: StreamSink<LogEntry>) {
+    // TODO: Move in a different spot
+    init_tracing(LevelFilter::DEBUG, false).expect("Logger to initialise");
     LOG_STREAM_SINK.set(sink);
 }
 
 pub fn log(msg: &str) {
+    tracing::debug!(msg);
     LOG_STREAM_SINK
         .try_get()
-        .expect("logging to be initialised")
+        .expect("StreamSink from Flutter to be initialised")
         .add(LogEntry {
             msg: msg.to_string(),
         });
