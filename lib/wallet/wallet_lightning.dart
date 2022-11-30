5d800dd90029c57df3350aab5ddab3db680a21d8
Richard Holzeis <richard@holzeis.me>
2022-11-30 21:38:48 +0100

    chore: Remove unused class

--- lib/wallet/wallet_lightning.dart
+++ lib/wallet/wallet_lightning.dart
@@ -1,19 +1,16 @@
 import 'package:f_logs/model/flog/flog.dart';
-import 'package:flutter/material.dart' hide Divider;
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ten_ten_one/balance.dart';
 import 'package:provider/provider.dart';
-import 'package:ten_ten_one/cfd_trading/cfd_trading.dart';
 import 'package:ten_ten_one/cfd_trading/cfd_trading_change_notifier.dart';
 import 'package:ten_ten_one/cfd_trading/validation_error.dart';
 import 'package:ten_ten_one/main.dart';
 import 'package:ten_ten_one/models/balance_model.dart';
-import 'package:ten_ten_one/models/service_model.dart';
 import 'package:ten_ten_one/wallet/close_channel.dart';
 import 'package:ten_ten_one/wallet/payment_history_list_item.dart';
 import 'package:ten_ten_one/wallet/receive.dart';
 import 'package:ten_ten_one/wallet/send.dart';
-import 'package:ten_ten_one/wallet/service_card.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 import 'package:ten_ten_one/menu.dart';
@@ -135,26 +132,5 @@ class _WalletLightningState extends State<WalletLightning> {
     } catch (error) {
       FLog.error(text: "Failed to get balances:" + error.toString());
     }
-  }
-}
-
-class ServiceNavigation extends StatelessWidget {
-  const ServiceNavigation({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 110.0,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: [
-          GestureDetector(
-            onTap: () => {GoRouter.of(context).go(CfdTrading.route)},
-            child: const ServiceCard(Service.trade),
-          ),
-          const ServiceCard(Service.dca),
-        ],
-      ),
-    );
   }
 }
