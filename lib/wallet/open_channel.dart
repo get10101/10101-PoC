72221839845f47ca5ac04ba0451cc09b27254b91
Daniel Karzel <daniel@comit.network>
2022-12-05 10:23:11 +1100

    Form validation for opening channel
    Du to trouble using the `controller` for setting values in combination with the validation I opted for setting an initial value instead and adding a specific formatter.
    The formatter combines well with the form validation - we should not just overwrite values that the user enters, but give the user an initial value and tell the user what is wrong if the form validation fails.
    In order to get the initial value formatted we call the formatter specifically (unfortunately it is not called for the initial value out of the box).
    
    Instead of only allowing channel open on a confirmed balance we allow it based on the external pending balance.
    If the pending balance is not `0` we use the pending balance, otherwise the confirmed one to determine the maximum amount the taker can use.

--- lib/wallet/open_channel.dart
+++ lib/wallet/open_channel.dart
@@ -2,7 +2,6 @@ import 'package:f_logs/f_logs.dart';
 import 'package:flutter/material.dart' hide Divider;
 import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:ten_ten_one/balance.dart';
 
@@ -25,126 +24,155 @@ class OpenChannel extends StatefulWidget {
 }
 
 class _OpenChannelState extends State<OpenChannel> {
-  final controller = TextEditingController();
   int takerChannelAmount = 0;
-  bool validForm = true;
   bool submitting = false;
+
+  final _formKey = GlobalKey<FormState>();
+  static const minValue = 1000;
 
   @override
   void initState() {
     super.initState();
-    final bitcoinBalance = context.read<BitcoinBalance>();
-    takerChannelAmount = bitcoinBalance.confirmed.asSats.clamp(0, OpenChannel.maxChannelAmount);
-    controller.text = NumberFormat().format(takerChannelAmount);
-    controller.addListener(() {
-      setState(() {
-        validForm = controller.text.isNotEmpty;
-      });
-    });
   }
 
   @override
   void dispose() {
-    controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final bitcoinBalance = context.watch<BitcoinBalance>();
+    final totalBalance = bitcoinBalance.confirmed.asSats +
+        bitcoinBalance.pendingInternal.asSats +
+        bitcoinBalance.pendingExternal.asSats;
+
+    final maxTakerChannelAmount = (totalBalance).clamp(0, OpenChannel.maxChannelAmount);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Open Channel'),
       ),
-      body: SafeArea(
-          child: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: const [
-                  Balance(balanceSelector: BalanceSelector.bitcoin),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: EdgeInsets.only(left: 20.0, right: 20.0),
-                    child: Divider(),
-                  )
-                ]),
-                const SizedBox(height: 10),
-                const Center(child: Icon(Icons.private_connectivity_outlined, size: 80)),
-                const SizedBox(height: 10),
-                ListTile(
-                    leading: addCircle("1"),
-                    title: const Text("Fund your bitcoin wallet"),
-                    subtitle: const Text(
-                        "In order to establish a Lightning channel between 10101 and you we need to lock some bitcoin on chain.",
-                        style: TextStyle(color: Colors.grey))),
-                const SizedBox(height: 15),
-                ListTile(
-                    leading: addCircle("2"),
-                    title: const Text("Enter the channel amount"),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                            "Define the amount of bitcoin you want to lock into the Lightning channel. 10101 will double it for great inbound liquidity. Max channel size is currently $takerChannelAmount sats.",
-                            style: const TextStyle(color: Colors.grey)),
-                        TextFormField(
-                          controller: controller,
-                          readOnly: submitting,
-                          keyboardType: TextInputType.number,
-                          decoration: const InputDecoration(
-                            border: UnderlineInputBorder(),
-                          ),
-                          inputFormatters: <TextInputFormatter>[
-                            FilteringTextInputFormatter.digitsOnly
-                          ],
-                          onChanged: (text) {
-                            setState(() {
-                              if (text != "") {
-                                takerChannelAmount =
-                                    int.parse(text).clamp(0, OpenChannel.maxChannelAmount);
-                                controller.text = NumberFormat().format(takerChannelAmount);
-                                controller.selection = TextSelection.fromPosition(
-                                    TextPosition(offset: controller.text.length));
-                              } else {
-                                takerChannelAmount = 0;
+      body: Form(
+        key: _formKey,
+        child: SafeArea(
+            child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: const [
+                    Balance(balanceSelector: BalanceSelector.bitcoin),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                      child: Divider(),
+                    )
+                  ]),
+                  const SizedBox(height: 10),
+                  const Center(child: Icon(Icons.private_connectivity_outlined, size: 80)),
+                  const SizedBox(height: 10),
+                  ListTile(
+                      leading: addCircle("1"),
+                      title: const Text("Fund your bitcoin wallet"),
+                      subtitle: const Text(
+                          "In order to establish a Lightning channel between 10101 and you we need to lock some bitcoin on chain.",
+                          style: TextStyle(color: Colors.grey))),
+                  const SizedBox(height: 15),
+                  ListTile(
+                      leading: addCircle("2"),
+                      title: const Text("Enter channel outbound liquidity"),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                              "Define the amount of bitcoin you want to lock into the Lightning channel. 10101 will double it for great inbound liquidity. The max amount you can use is currently $maxTakerChannelAmount sats.",
+                              style: const TextStyle(color: Colors.grey)),
+                          TextFormField(
+                            initialValue: ThousandsSeparatorInputFormatter()
+                                .formatEditUpdate(TextEditingValue.empty,
+                                    TextEditingValue(text: maxTakerChannelAmount.toString()))
+                                .text,
+                            readOnly: submitting,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              border: UnderlineInputBorder(),
+                            ),
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.digitsOnly,
+                              ThousandsSeparatorInputFormatter(),
+                            ],
+                            onChanged: (value) {
+                              setState(() {
+                                try {
+                                  takerChannelAmount = int.parse(value.replaceAll(",", ""));
+                                } on Exception {
+                                  takerChannelAmount = 0;
+                                }
+                              });
+
+                              _formKey.currentState!.validate();
+                            },
+                            validator: (value) {
+                              if (value == null) {
+                                return "Enter channel outbound liquidity";
                               }
-                            });
-                          },
-                        )
-                      ],
-                    )),
-                const SizedBox(height: 15),
-                ListTile(
-                    leading: addCircle("3"),
-                    title: const Text("Select your maker"),
-                    subtitle: const Text(
-                        "In this version you can only trade with 10101, but in the future you will be able to choose who to connect to.",
-                        style: TextStyle(color: Colors.grey))),
-                const SizedBox(height: 15),
-                ListTile(
-                    leading: addCircle("4"),
-                    title: const Text("Ready!"),
-                    subtitle: const Text(
-                        "Hit the 'Open Channel' button and the channel will be created in a few moments.",
-                        style: TextStyle(color: Colors.grey))),
-                const SizedBox(height: 15),
-              ],
+
+                              try {
+                                int intVal = int.parse(value.replaceAll(",", ""));
+
+                                if (intVal > totalBalance) {
+                                  return "Insufficient balance ($totalBalance)";
+                                }
+
+                                if (intVal > maxTakerChannelAmount) {
+                                  return "Max channel amount is $maxTakerChannelAmount";
+                                }
+                                if (intVal < minValue) {
+                                  return "Min channel amount is $minValue";
+                                }
+                              } on Exception {
+                                return "Enter channel outbound liquidity";
+                              }
+
+                              return null;
+                            },
+                          )
+                        ],
+                      )),
+                  const SizedBox(height: 15),
+                  ListTile(
+                      leading: addCircle("3"),
+                      title: const Text("Select your maker"),
+                      subtitle: const Text(
+                          "In this version you can only trade with 10101, but in the future you will be able to choose who to connect to.",
+                          style: TextStyle(color: Colors.grey))),
+                  const SizedBox(height: 15),
+                  ListTile(
+                      leading: addCircle("4"),
+                      title: const Text("Ready!"),
+                      subtitle: const Text(
+                          "Hit the 'Open Channel' button and the channel will be created in a few moments.",
+                          style: TextStyle(color: Colors.grey))),
+                  const SizedBox(height: 15),
+                ],
+              ),
             ),
-          ),
-          Padding(
-              padding: const EdgeInsets.only(right: 20.0),
-              child: Container(
-                alignment: Alignment.bottomRight,
-                child: SubmitButton(
-                  onPressed: openChannel,
-                  label: 'Open Channel',
-                  isButtonDisabled: !validForm,
-                ),
-              ))
-        ],
-      )),
+            Padding(
+                padding: const EdgeInsets.only(right: 20.0),
+                child: Container(
+                  alignment: Alignment.bottomRight,
+                  child: SubmitButton(
+                    onPressed: openChannel,
+                    label: 'Open Channel',
+                    isButtonDisabled:
+                        _formKey.currentState == null ? false : !_formKey.currentState!.validate(),
+                  ),
+                ))
+          ],
+        )),
+      ),
     );
   }
 
@@ -177,5 +205,48 @@ class _OpenChannelState extends State<OpenChannel> {
         style: const TextStyle(color: Colors.white, fontSize: 20),
       )),
     );
+  }
+}
+
+class ThousandsSeparatorInputFormatter extends TextInputFormatter {
+  static const separator = ',';
+
+  @override
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.text.isEmpty) {
+      return newValue.copyWith(text: '');
+    }
+
+    // Handle "deletion" of separator character
+    String oldValueText = oldValue.text.replaceAll(separator, '');
+    String newValueText = newValue.text.replaceAll(separator, '');
+
+    if (oldValue.text.endsWith(separator) && oldValue.text.length == newValue.text.length + 1) {
+      newValueText = newValueText.substring(0, newValueText.length - 1);
+    }
+
+    // Only process if the old value and new value are different
+    if (oldValueText != newValueText) {
+      int selectionIndex = newValue.text.length - newValue.selection.extentOffset;
+      final chars = newValueText.split('');
+
+      String newString = '';
+      for (int i = chars.length - 1; i >= 0; i--) {
+        if ((chars.length - 1 - i) % 3 == 0 && i != chars.length - 1) {
+          newString = separator + newString;
+        }
+        newString = chars[i] + newString;
+      }
+
+      return TextEditingValue(
+        text: newString.toString(),
+        selection: TextSelection.collapsed(
+          offset: newString.length - selectionIndex,
+        ),
+      );
+    }
+
+    // If the new value and old value are the same, just return as-is
+    return newValue;
   }
 }
