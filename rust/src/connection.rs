393e8d4fd730f5b397e54256a051f7e88406b49f
Richard Holzeis <richard@holzeis.me>
2022-12-09 14:53:24 +0100

    fixup! chore: Clean up connection logic
    not just a reorganisation of the code, but also a refactoring. e.g. we are now awaiting the connection_closed_future to see if we have to reconnect (instead of polling the channel state constantly).

--- rust/src/connection.rs
+++ rust/src/connection.rs
@@ -1,79 +1,50 @@
 use crate::config;
 use crate::lightning::PeerManager;
-use crate::wallet::is_first_channel_usable;
+use bdk::bitcoin::secp256k1::PublicKey;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::task::JoinHandle;
 
-pub struct Connection {
-    peer_manager: Arc<PeerManager>,
-}
-
 pub fn spawn(peer_manager: Arc<PeerManager>) -> JoinHandle<()> {
-    let connection = Connection { peer_manager };
-
     // keep connection with maker alive!
     tokio::spawn(async move {
-        let mut connected = false;
+        let peer_info = config::maker_peer_info();
         loop {
-            if !connected || !is_first_channel_usable() {
-                connected = connection.connect().await;
+            tracing::info!("Connecting to {peer_info}");
+            match lightning_net_tokio::connect_outbound(
+                Arc::clone(&peer_manager),
+                peer_info.pubkey,
+                peer_info.peer_addr,
+            )
+            .await
+            {
+                Some(connection_closed_future) => {
+                    let mut connection_closed_future = Box::pin(connection_closed_future);
+                    while !is_connected(&peer_manager, peer_info.pubkey) {
+                        if futures::poll!(&mut connection_closed_future).is_ready() {
+                            tracing::warn!("Peer disconnected before we finished the handshake! Retrying in 5 seconds.");
+                            tokio::time::sleep(Duration::from_secs(5)).await;
+                            return;
+                        }
+                        tokio::time::sleep(Duration::from_secs(5)).await;
+                    }
+                    tracing::info!("Successfully connected to {peer_info}");
+                    connection_closed_future.await;
+                    tracing::warn!("Lost connection to maker, retrying immediately.")
+                }
+                None => {
+                    tracing::warn!("Failed to connect to maker! Retrying in 5 seconds.");
+                    // waiting for 5 seconds before retrying to connect to the maker
+                    tokio::time::sleep(Duration::from_secs(5)).await;
+                }
             }
-
-            // looping here indefinitely to keep the connection with the maker alive.
-            tokio::time::sleep(std::time::Duration::from_secs(5)).await;
         }
     })
 }
 
-impl Connection {
-    async fn connect(&self) -> bool {
-        let peer_info = config::maker_peer_info();
-        for node_pubkey in self.peer_manager.get_peer_node_ids() {
-            if node_pubkey == peer_info.pubkey {
-                tracing::trace!("Already connected to maker {peer_info}.");
-                return true;
-            }
-        }
-
-        tracing::debug!("Connecting to {peer_info}");
-
-        match lightning_net_tokio::connect_outbound(
-            Arc::clone(&self.peer_manager),
-            peer_info.pubkey,
-            peer_info.peer_addr,
-        )
-        .await
-        {
-            Some(connection_closed_future) => {
-                let mut connection_closed_future = Box::pin(connection_closed_future);
-                loop {
-                    match futures::poll!(&mut connection_closed_future) {
-                        std::task::Poll::Ready(_) => {
-                            tracing::warn!("Peer disconnected before we finished the handshake!");
-                            return false;
-                        }
-                        std::task::Poll::Pending => {}
-                    }
-                    // Avoid blocking the tokio context by sleeping a bit
-                    match self
-                        .peer_manager
-                        .get_peer_node_ids()
-                        .iter()
-                        .find(|id| **id == peer_info.pubkey)
-                    {
-                        Some(_) => {
-                            tracing::info!("Successfully connected to maker {peer_info}");
-                            return true;
-                        }
-                        None => tokio::time::sleep(Duration::from_millis(10)).await,
-                    }
-                }
-            }
-            None => {
-                tracing::warn!("Failed to connect to maker!");
-                false
-            }
-        }
-    }
+fn is_connected(peer_manager: &Arc<PeerManager>, pubkey: PublicKey) -> bool {
+    peer_manager
+        .get_peer_node_ids()
+        .iter()
+        .any(|id| *id == pubkey)
 }
